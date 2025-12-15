a4e9d6aa0e7dbfc2674c3b0ad9934938c5458f40
Le Anh Khoa <khoale22082004@gmail.com>
2025-12-15 14:48:46 +0700

    zmc

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/HomeComponent/Home";
 import Memories from "./components/MemmoriesComponet/Memories";
 export default function App() {
   return (
-    <>
-      {/* <Home></Home> */}
-      <Memories></Memories>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/memories" element={<Memories />} />
+      </Routes>
+    </Router>
   );
 }
