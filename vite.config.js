086139808294f1334dcc5b71b2d8098b33be442d
Le Anh Khoa <khoale22082004@gmail.com>
2025-12-17 16:15:11 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_PATH || "/CodeOfPic ",
 });
